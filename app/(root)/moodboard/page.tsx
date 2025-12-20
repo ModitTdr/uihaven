14e201495fd0f9cd9d2cc3080471f9b44a7e3fb7
ModitTdr <modittuladhar@gmail.com>
2025-12-20 11:33:12 +0545

    fix: imgRatio & removedGridSnap

--- app/(root)/moodboard/page.tsx
+++ app/(root)/moodboard/page.tsx
@@ -1,26 +1,10 @@
 "use client";
 import DragableImg from "@/components/DragableImg";
 import Dropzone, { imageObjectType } from "@/components/Dropzone";
-import { Info } from "lucide-react";
-import { useEffect, useRef, useState } from "react";
-import toast from "react-hot-toast";
+import { useState } from "react";
 
 const Page = () => {
   const [images, setImages] = useState<imageObjectType[]>([]);
-  const hasShownToast = useRef(false);
-
-  useEffect(() => {
-    if (images.length === 1 && !hasShownToast.current) {
-      toast(
-        <span className="text-sm">Images are resizeable</span>,
-        {
-          icon: <Info size={18} />,
-        }
-      );
-      hasShownToast.current = true;
-    }
-  }, [images.length]);
-
   const delImg = (id: string) => {
     setImages((prev) => prev.filter((img) => id !== img.id));
   };
