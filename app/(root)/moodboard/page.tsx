35fa32447e9bd93da1ea64ebb2a933d98956a4ef
ModitTdr <modittuladhar@gmail.com>
2025-12-20 09:27:46 +0545

    fix: imgDel works on touch devices& added ResizeIcon

--- app/(root)/moodboard/page.tsx
+++ app/(root)/moodboard/page.tsx
@@ -26,16 +26,16 @@ const Page = () => {
   };
 
   return (
-    <div className="min-h-[90dvh] flex justify-start items-center px-6 py-2 flex-col gap-3 mx-auto">
+    <div className="min-h-[90dvh] flex justify-start items-center px-2 py-2 flex-col gap-3 mx-auto">
       <Dropzone images={images} setItems={setImages} disabled={images.length >= 8} />
-      <div className="flex-1 w-full border border-foreground/20 rounded-md overflow-scroll relative">
+      <div className="flex-1 w-full border border-foreground/20 rounded-md overflow-hidden relative">
         {images.map((image) => (
           <DragableImg imgObj={image} key={image.id} delImg={delImg} />
         ))}
       </div>
 
-      <span className="text-xs text-muted-foreground">
-        Images are resizable and draggable. Double click an image to adjust its layer order
+      <span className="text-xs text-muted-foreground text-center">
+        Images are resizable and draggable.
       </span>
     </div>
   );
--- components/DragableImg.tsx
+++ components/DragableImg.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Rnd } from "react-rnd";
 import { imageObjectType } from "./Dropzone";
-import { X } from "lucide-react";
+import { ArrowDownRight, X } from "lucide-react";
 
 interface DragableImgProp {
   imgObj: imageObjectType;
@@ -25,14 +25,34 @@ const DragableImg = ({
         width: 300,
         height: 300,
       }}
-      style={{ position: "absolute", padding: "8px 0px 8px 18px" }}
-      enableResizing={{
-        bottom: true,
-        right: true,
-        bottomRight: true,
-      }}
+      style={{ position: "absolute", background: "rgba(0,0,0,0.04)" }}
       maxWidth={350}
       maxHeight={400}
+      enableResizing={{
+        bottomRight: true,
+      }}
+      resizeHandleComponent={{
+        bottomRight: (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '20px',
+              height: '20px',
+              position: 'absolute',
+              bottom: '10px',
+              right: '10px',
+              backgroundColor: '#4CAF50',
+              borderRadius: '30% 30% 0% 30%',
+              cursor: 'se-resize',
+              zIndex: 2,
+            }}
+          >
+            <ArrowDownRight size={14} color="white" />
+          </div>
+        ),
+      }}
     >
       <div className="relative w-full h-full" >
         <img
@@ -42,10 +62,11 @@ const DragableImg = ({
           className="w-full h-full object-stretch pointer-events-none select-none"
         />
         <div
-          className="absolute -top-2 -left-2 bg-red-500/80 rounded-lg text-white p-1 cursor-pointer shadow-md backdrop-blur-xl"
+          className="absolute top-0 left-0 bg-red-500/80 rounded-tl-none rounded-lg text-white p-1 cursor-pointer shadow-md backdrop-blur-xl"
           onClick={() => delImg(imgObj.id)}
+          onTouchStart={() => delImg(imgObj.id)}
         >
-          <X size={16} className="pointer-events-none" />
+          <X size={16} />
         </div>
       </div>
     </Rnd>
